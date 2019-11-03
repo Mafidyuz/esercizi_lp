03e7035060672e1e0d0e9d1c5cdd7a61c85085ad
Mafidyuz <mario.petruccelli@studenti.unimi.it>
2019-11-03 23:58:17 +0100

    Transpose in 2 righe <3

--- OCaml/lab_2/es_5/Matrix.ml
+++ OCaml/lab_2/es_5/Matrix.ml
@@ -12,14 +12,13 @@ module Matrix = struct
     let ( **. ) a k =
         match a with
         M x -> M (List.map (fun y -> (List.map (fun z -> z * k)) y ) x );;
-
-    let make_column a = 
-        match a with
-        M (x::xs) -> List.map (fun y -> y ) x 
-
-    let rec transpose a = 
-        match a with
-        M a -> match a with
-            | x::xs -> List.map ( fun y -> y:: ) x
+    
+    let transpose = 
+        let rec transpose ?(length=0) = function
+            x::xs -> List.map2 (fun a b -> a@b) (List.map (fun y -> [y]) x) (transpose ~length:(List.length x) xs)
+            | [] -> Array.make length [] |> Array.to_list
+        in function 
+             M x -> M(transpose x);;
+        
 
 end;;   
